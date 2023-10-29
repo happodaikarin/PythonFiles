86a31e6b05a2d4c3682ca8ff77c5e1d72f927277
Ramses Hidalgo <rambandan@gmail.com>
2023-10-29 06:27:24 -0500

    search email code line 33

--- inheritance.py
+++ inheritance.py
@@ -29,6 +29,7 @@ c1 = Contact("John A", "johna@gmail.com")
 c2 = Contact("John B", "johnb@gmail.com")
 c3 = Contact("Jenna C", "jenna@gmail.com")
 print([c.name for c in Contact.all_contact.search('John')])
+print([c.email for c in Contact.all_contact.search('John')])
 
 
 
